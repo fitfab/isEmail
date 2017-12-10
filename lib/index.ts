17c05402279b4466d7df3d98558ebe4c04de63b9
Miguel Julio <fitfab@yahoo.com>
2017-12-10 16:55:25 -0500

    setup jest unit test (#1)

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@
  * 
  * @param str 
  */
-export function isEmail(str:string):boolean {
+export default function isEmail(str:string):boolean {
     // Verify : https://regex101.com/r/VBGyXV/1
     const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
     return re.test(str)
